4a74df5b9ef8495b2115b480d3dbcdba6a8a57b3
Tal <talj.gelfand@gmail.com>
2020-08-09 13:38:05 +0300

    Навигация по DOM - элементам, data-атрибуты, преимущество for/of

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes); // узлы, которые являются детьми body
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode); // получить родителя элемента с id current
+// console.log(document.querySelector('#current').parentNode.parentNode); // получить родителя родителя элемента с id current
+// console.log(document.querySelector('#current').parentElement);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+
+    console.log(node);
+} // перебрать и вывести все узлы из body кроме текстовых
